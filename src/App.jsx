32d16e0e82dae10a1979328e8dc5a073c7a299d9
akira-odgr <177723350+akira-odgr@users.noreply.github.com>
2025-03-31 17:46:42 +0900

    portfolio-reactの作成を開始しました。

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import { Header } from "./component/header/Header";
+
+export const App = () => {
+    return (
+        <>
+            <Header />
+        </>
+    );
+};
+
+export default App;
